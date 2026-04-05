976cee07f3799ba5ffedc8c61434c0399687bc59
paul <razeryutux@gmail.com>
2026-04-05 00:08:32 -0400

    funcion gestionar inscritos agregada y funcional

--- gui/ventana_principal.py
+++ gui/ventana_principal.py
@@ -131,14 +131,6 @@ class VentanaPrincipal:
             else:
                 messagebox.showerror('ERROR','No se ha seleccionado ningun alumno')
 
-
-            
-
-
-
-
-
-      
         columnas = self.log.obtener_datos_columnas()
         vista_tabla = ttk.Treeview(frame_tabla,columns=columnas,show="headings",yscrollcommand=scroll_bar.set)
         datos_columnas = self.log.obtener_datos_totales_tabla()
@@ -166,8 +158,55 @@ class VentanaPrincipal:
 
     
 
-    
+    def gestionar_inscritos(self):
+        def modificar_estatus():
+            
+            item_selec = tabla.selection()
+            if item_selec:
+                datos = tabla.item(item_selec[0])['values']
+                self.log.modificar_datos_alumno(cedula=datos[0],dato_ob='estatus_pago',dato_nuevo='')
+                messagebox.showinfo("EXITO",f"Alumno {datos[1]} modificado")
+            else:
+                messagebox.showerror("ERROR","NO HAY NINGUN ALUMNO SELECCIONADO")
+                
+                
+
+
+        sub_ventana = tk.Toplevel()
+        contenedor = tk.Frame(sub_ventana)
+        contenedor.pack(expand=True,fill='both',padx=20,pady=20)
+
+        columnas_nombre = ['cedula','nombre','carrera','estatus']
+
+
+        tabla = ttk.Treeview(contenedor,columns=columnas_nombre,show='headings')
+        datos = self.log.obtener_datos_totales_tabla()
         
+
+        for col in columnas_nombre:
+            tabla.heading(col,text=col.upper())
+            tabla.column(col,width=120,anchor='center')
+
+        #eliminar datos residuales
+        for item in tabla.get_children():
+            tabla.delete(item)
+
+        #insertar datos traidos de la base de datos
+        for alumno in datos:
+            tabla.insert("","end",values=(alumno[0],alumno[1],alumno[4],alumno[5]))
+
+
+        scroll_bar = ttk.Scrollbar(contenedor,orient='vertical')
+        scroll_bar.config(command=tabla.yview)
+        tabla.pack(side='left',expand=True,fill='both')
+        scroll_bar.pack(side='right',fill='y')
+
+
+        tk.Button(sub_ventana,text="CAMBIAR ESTADO",command=lambda: modificar_estatus()).pack(side='bottom',pady=20)
+        
+        
+
+
 
 
     def menu_principal(self):
@@ -177,10 +216,12 @@ class VentanaPrincipal:
 
         tk.Button(self.ventana_principal,text="VISUALIZAR REGISTRO",width=25,command=lambda: self.visualizar_registros()).pack(pady=10)
 
-        tk.Button(self.ventana_principal,text="GESTION DE REGISTROS",width=25).pack(pady=10)
+        tk.Button(self.ventana_principal,text="GESTION DE REGISTROS",width=25,command=lambda: self.gestionar_inscritos()).pack(pady=10)
 
         tk.Button(self.ventana_principal,text="CERRAR",width=25,command=self.ventana_principal.destroy).pack(pady=10)
 
     def bucle_principal(self):
         self.ventana_principal.mainloop()
 
+
+#TODO: agregar funcion de actualizar tablas
